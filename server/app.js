120a20633c8b99c85c383b900d45d12eae1dfe77
Naman Bhugra <namanbhugra7@gmail.com>
2026-02-01 23:57:55 +0530

    update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
-
 app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/projects', require('./routes/projectRoutes'));
