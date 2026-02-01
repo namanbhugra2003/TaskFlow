3bbdbb65951faae2ef550b47e0af714c88f880d9
Naman Bhugra <namanbhugra7@gmail.com>
2026-02-02 01:34:52 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,7 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: 'https://task-flow-lac-one.vercel.app/', credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api/auth', require('./routes/authRoutes'));
