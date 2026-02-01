0cf36fcf337b65d703d50e98e9a1b3585f17c674
Naman Bhugra <namanbhugra7@gmail.com>
2026-02-02 00:48:33 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,15 @@ const { Sequelize } = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "mysql",
   logging: false,
+  pool: {
+    max: 1,
+    min: 0,
+    idle: 0,
+    acquire: 30000,
+  },
+  dialectOptions: {
+    connectTimeout: 60000,
+  },
 });
 
 module.exports = sequelize;
