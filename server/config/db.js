9b351087722007d28424a8e01164f1f8bc62d07c
Naman Bhugra <namanbhugra7@gmail.com>
2026-02-02 00:38:42 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,8 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,   // ✅ important for Railway
-    dialect: "mysql",
-    logging: false,
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "mysql",
+  logging: false,
+});
 
 module.exports = sequelize;
