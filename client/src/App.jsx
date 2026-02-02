5467cfd581fe9832f358ff40f3a6050408d8ea3c
Naman Bhugra <namanbhugra7@gmail.com>
2026-02-02 08:35:43 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
 import ManagerDashboard from "./pages/ManagerDashboard";
@@ -7,29 +7,36 @@ import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      <Route path="/admin" element={
-  <ProtectedRoute role="admin">
-    <AdminDashboard />
-  </ProtectedRoute>
-} />
+    <Routes>
+      <Route path="/" element={<Login />} />
 
-<Route path="/manager" element={
-  <ProtectedRoute role="manager">
-    <ManagerDashboard />
-  </ProtectedRoute>
-} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute role="admin">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      />
 
-<Route path="/user" element={
-  <ProtectedRoute role="user">
-    <UserDashboard />
-  </ProtectedRoute>
-} />
+      <Route
+        path="/manager"
+        element={
+          <ProtectedRoute role="manager">
+            <ManagerDashboard />
+          </ProtectedRoute>
+        }
+      />
 
-      </Routes>
-    </BrowserRouter>
+      <Route
+        path="/user"
+        element={
+          <ProtectedRoute role="user">
+            <UserDashboard />
+          </ProtectedRoute>
+        }
+      />
+    </Routes>
   );
 }
 
