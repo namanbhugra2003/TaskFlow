a1ef58f98bd52cf2fc9d1e0c071ab8a01818d2e6
Naman Bhugra <namanbhugra7@gmail.com>
2026-02-02 08:00:43 +0530

    Wrap App component with BrowserRouter and StrictMode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,14 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
