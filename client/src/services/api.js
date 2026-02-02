fcef6be932798ade92e42936843d32ad9e7d32cf
Naman Bhugra <namanbhugra7@gmail.com>
2026-02-02 08:45:10 +0530

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://taskflow-fggn.onrender.com",
+  baseURL: "https://taskflow-fggn.onrender.com/api",
   withCredentials: true,
 });
 
 export default api;
+
+// tasks
 export const getMyTasks = () => api.get("/tasks/my");
 export const updateTask = (id, data) => api.put(`/tasks/${id}`, data);
